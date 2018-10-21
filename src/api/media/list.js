574cd5853a291d6d13eb67990107d9199830f0dd
Joshua <joshstockin@gmail.com>
2018-10-20 22:30:07 -0500

    Add list behavior

--- src/api/media/list.js
+++ src/api/media/list.js
@@ -1,1 +1,36 @@
-module.exports = function () {};
+const responseSetting = require("../../responseSetting.js");
+
+module.exports = function(request, headers, response, mySQLconnection) {
+	let count = parseInt(headers['count']);
+	if (count.toString() == 'NaN') {
+		let htmlCode = 400;
+		responseSetting.setResponseFullHTML(response, htmlCode);
+		response.end();
+		return true;
+	}
+	if (count > 100) {
+		let htmlCode = 400;
+		responseSetting.setResponseFullHTML(response, htmlCode);
+		response.end();
+		return true;
+	}
+
+	let query = "SELECT * FROM joshuas3.images ORDER BY dateAdded DESC LIMIT ?"
+	mySQLconnection.query(query, [count], function(err, results) {
+		if (err) {
+			let htmlCode = 400;
+			responseSetting.setResponseFullHTML(response, htmlCode);
+			response.end();
+			return true;
+		}
+		let listOfIds = [];
+		results.forEach(function (row) {
+			listOfIds.push(row.id);
+		});
+
+		response.writeHead(200, 'application/json');
+		response.write(JSON.stringify(listOfIds));
+		response.end();
+		return true;
+	});
+}
