23e736454854bf1f9e601e6ea1683cf8021a3b04
Joshua <joshstockin@gmail.com>
2018-10-26 21:32:24 -0500

    Fix MySQL prolonged connection issue

--- src/mySQLconnection.js
+++ src/mySQLconnection.js
@@ -1,22 +1,30 @@
 const mysql = require("mysql");
 const mySqlConfig = require("../mySqlConfig.json");
-var mySQLconnection;
-function handleMySqlConnectionLoss() {
-	mySQLconnection = mysql.createConnection(mySqlConfig);
+var pool = mysql.createPool(mySqlConfig);
 
-	mySQLconnection.connect(function (err) {
-		if (err) {
-			console.log("Error when establishing MySQL connection: ", err);
-			setTimeout(handleMySqlConnectionLoss, 2000);
+
+module.exports = {
+	query: function(){
+		var sql_args = [];
+		var args = [];
+		for(var i=0; i<arguments.length; i++){
+			args.push(arguments[i]);
 		}
-	});
-	mySQLconnection.on('error', function(err){
-		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-			handleMySqlConnectionLoss();
-		} else {
-			throw err;
-		}
-	});
+		var callback = args[args.length-1]; //last arg is callback
+		pool.getConnection(function(err, connection) {
+			if(err) {
+				return callback(err);
+			}
+			if(args.length > 2){
+				sql_args = args[1];
+			}
+			connection.query(args[0], sql_args, function(err, results) {
+				connection.release(); // always put connection back in pool after last query
+				if(err){
+					return callback(err);
+				}
+				callback(null, results);
+			});
+		});
+	}
 }
-handleMySqlConnectionLoss();
-module.exports = mySQLconnection;
