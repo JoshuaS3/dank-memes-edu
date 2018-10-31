ae8a4e937372dd009d3ac543256d22f83783434f
Joshua <joshstockin@gmail.com>
2018-10-30 21:48:49 -0500

    Update JWT secret management

--- src/JWTsecret.js
+++ src/JWTsecret.js
@@ -1,9 +1,87 @@
+const fs = require("fs");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const logger = require("./logger.js");
+
+let pathToSecrets = path.join(__dirname, "../JWTsecrets.json");
+
 function secretKeyGen() {
 	var secretKey = "";
 	var possibleChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[]\\{}|;':\",./<>?`~!@#$%^&*()-=_+\ ";
 	for (var i = 0; i < 512; i++)
 		secretKey += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
-	return secretKey;
+	let now = new Date();
+	logger.d("JsonWebToken", "New secret key generated");
+	return {secret: secretKey, dateCreated: now};
 }
 
-module.exports = secretKeyGen();
+let secrets = JSON.parse(fs.readFileSync(pathToSecrets).toString());
+
+const one_day = 10*1000;
+
+function checkForOldSecrets() {
+	let now = new Date();
+	for (var secretIndex in secrets) {
+		secret = secrets[secretIndex];
+		if ((now - secret.dateCreated) > one_day) {
+			let position = secrets.indexOf(secret);
+			secrets.slice(position, position);
+			logger.d("JsonWebToken","Removed a secret key older than 24 hours");
+		}
+	};
+	if (secrets.length == 0) {
+		secrets.unshift(secretKeyGen());
+	}
+	fs.writeFileSync(pathToSecrets, JSON.stringify(secrets));
+}
+
+function verify(data, callback) {
+	checkForOldSecrets();
+	
+	let error = new Error('NoSecretError');
+	error.name = 'NoSecretError';
+	error.message = 'no secrets generated';
+	error.date = new Date();
+	let success = false;
+
+	for (var secretIndex in secrets) {
+		if (success) return;
+		let secret = secrets[secretIndex];
+		try {
+			jwt.verify(data, secret.secret, function(err, verified) {
+				if (err) {
+					throw err;
+				}
+				logger.v("JsonWebToken", "Successful verification");
+				callback(null, verified); // no error caught, correct signature and payload
+				success = true;
+				return;
+			});
+		} catch (e) {
+			if (e.name == "TokenExpiredError") { // correct signature, just an old token
+				logger.v("JsonWebToken", "TokenExpiredError");
+				callback(e, null);
+				return;
+			}
+			error = e;
+		}
+	};
+	if (success) return;
+	logger.v("JsonWebToken", "JsonWebTokenError");
+	callback(error, null);
+}
+
+function sign(data) {
+	checkForOldSecrets();
+	return jwt.sign(data, secrets[0].secret);
+}
+
+
+checkForOldSecrets();
+
+
+
+module.exports = {
+	verify: verify,
+	sign: sign
+};
--- src/api/accounts/createToken.js
+++ src/api/accounts/createToken.js
@@ -1,7 +1,6 @@
 const logger = require("../../logger.js");
 const responseSetting = require("../../responseSetting.js");
-const jwt = require("jsonwebtoken");
-const JWTsecret = require("../../JWTsecret.js");
+const jwt = require("../../JWTsecret.js");
 const md5 = require("md5");
 const uuidv4 = require("uuid/v4");
 
@@ -14,8 +13,7 @@ module.exports = function(request, fullHeaders, response, truncatedUrl) {
 			accountCreation: true,
 			exp: Math.floor(Date.now() / 1000) + 3600,
 			uuid: uuidv4()
-		},
-		JWTsecret
+		}
 	)
 	responseJSON.data = allowCreationToken;
 	responseSetting.setResponseFullJSON(response, responseJSON);
