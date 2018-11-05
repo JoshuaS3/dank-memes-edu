4a67a5db4901beb8900f8b59793379723ca5c3c5
Joshua <joshstockin@gmail.com>
2018-11-04 20:50:39 -0600

    Async login state checking

--- static/js/loginState.js
+++ static/js/loginState.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
-	$.getJSON('/api/accounts/state', null, function(result) {
-		var loginState = result;
-	});
-});
+function checkLoginState(callback) {
+	let xmlHTTP = new XMLHttpRequest();
+	let url = "/api/accounts/state";
+
+	xmlHTTP.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			let loginState = JSON.parse(this.responseText);
+			callback(loginState);
+		}
+	}
+	xmlHTTP.open("GET", url, true);
+	xmlHTTP.send();
+}
