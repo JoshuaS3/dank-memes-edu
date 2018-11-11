83c0e415b9ad8d364205f3d056925ab3737bae71
Joshua <joshstockin@gmail.com>
2018-11-10 18:10:28 -0600

    Fix logout cookie setting

--- src/api/accounts/logout.js
+++ src/api/accounts/logout.js
@@ -5,7 +5,7 @@ module.exports = function(request, fullHeaders, response, truncatedUrl) {
 	let responseJSON = {};
 	responseJSON.success = true;
 	responseJSON.status = 200;
-	response.setHeader("Set-Cookie", [`authToken=${null}`]);
+	response.setHeader("Set-Cookie", `authToken=; expires=Thu, Jan 01 1970 00:00:00 UTC`);
 	responseSetting.setResponseFullJSON(response, responseJSON);
 	return;
 }
