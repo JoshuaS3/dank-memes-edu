d8ab4ea8a1cd3a71aa67da8f75ccdce6d7d9b7ff
Joshua <joshstockin@gmail.com>
2018-11-10 18:10:42 -0600

    Fix login state checking

--- src/api/accounts/state.js
+++ src/api/accounts/state.js
@@ -1,34 +1,39 @@
 const logger = require("../../logger.js");
 const responseSetting = require("../../responseSetting.js");
+const httpBodyParser = require("../../httpBodyParser.js");
 const jwt = require("../../JWTsecret.js");
 const cookie = require("cookie");
 
 module.exports = function(request, fullHeaders, response, truncatedUrl) {
 	let responseJSON = {};
-	let cookies = cookie.parse(fullHeaders.cookie || "");
-	if (!cookies.authToken) {
-		responseJSON.success = false;
-		responseJSON.status = 400;
-		responseJSON.message = "Not signed in";
-		responseSetting.setResponseFullJSON(response, responseJSON);
-		return;
-	}
-	jwt.verify(cookies.authToken, function(err, decoded) {
-		if (err) {
+
+	httpBodyParser(request, responseJSON, function(body) {
+		let cookies = cookie.parse(fullHeaders.cookie || "");
+		logger.i("asdf", JSON.stringify(cookies));
+		if (!cookies.authToken) {
 			responseJSON.success = false;
 			responseJSON.status = 400;
 			responseJSON.message = "Not signed in";
 			responseSetting.setResponseFullJSON(response, responseJSON);
 			return;
 		}
-		responseJSON.success = true;
-		responseJSON.status = 200;
-		responseJSON.message = "Signed in";
-		responseJSON.data = {
-			userId: decoded.userId,
-			displayName: decoded.displayName
-		}
-		responseSetting.setResponseFullJSON(response, responseJSON);
-		return;
+		jwt.verify(cookies.authToken, function(err, decoded) {
+			if (err) {
+				responseJSON.success = false;
+				responseJSON.status = 400;
+				responseJSON.message = "Not signed in";
+				responseSetting.setResponseFullJSON(response, responseJSON);
+				return;
+			}
+			responseJSON.success = true;
+			responseJSON.status = 200;
+			responseJSON.message = "Signed in";
+			responseJSON.data = {
+				userId: decoded.userId,
+				displayName: decoded.displayName
+			}
+			responseSetting.setResponseFullJSON(response, responseJSON);
+			return;
+		});
 	});
 }
