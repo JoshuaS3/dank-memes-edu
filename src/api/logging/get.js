963ad42ba52d9840e9e9d829e64af6e8229d5224
Joshua <joshstockin@gmail.com>
2018-11-11 18:38:59 -0600

    Fix log specifications

--- src/api/logging/get.js
+++ src/api/logging/get.js
@@ -12,7 +12,7 @@ module.exports = function(request, fullHeaders, response, truncatedUrl) {
 	logger.v("LoggingGet", "Processing request to fetch logs");
 
 	httpBodyParser(request, responseJSON, function(body) {
-		let filedate = body.filedate || fullHeaders.filedate || "INFO";
+		let filename = body.file || fullHeaders.file || null;
 		let verbosity = body.verbosity || fullHeaders.verbosity || "INFO";
 		let tag = body.tag || fullHeaders.tag || ".*";
 		let start = body.start || fullHeaders.start || 0;
@@ -54,7 +54,7 @@ module.exports = function(request, fullHeaders, response, truncatedUrl) {
 				responseSetting.setResponseFullJSON(response, responseJSON);
 				return;
 			}
-			fs.readdir(logger.logPath, function(err, files) {
+			fs.readFile(path.join(logger.logPath, filename), function(err, buffer) {
 				if (err) {
 					responseJSON.success = false;
 					responseJSON.status = 500;
@@ -63,81 +63,38 @@ module.exports = function(request, fullHeaders, response, truncatedUrl) {
 					responseSetting.setResponseFullJSON(response, responseJSON);
 					return;
 				}
-				files = files.map(function (fileName) {
-					return {
-						name: fileName,
-						time: fs.statSync(path.join(logger.logPath, fileName)).birthtime.getTime()
-					};
-				})
-				.sort(function (a, b) {
-					return b.time - a.time;
-				})
-				.map(function (file) {
-					if (file.name == ".gitkeep") {
-						return {name:null, time:null}
+				let filestat = fs.statSync(path.join(logger.logPath, filename));
+				let regex = new RegExp(`.*\\|\\ (?:${verbosity})\\ \\|\\ (?:${tag})\\ \\|\\ .*?(?:[\\n].*?)*(?={EOT})`, "g");
+				let fullLog = buffer.toString();
+				let m;
+				let fullResponse = [];
+
+				while ((m = regex.exec(fullLog)) !== null) {
+					if (m.index === regex.lastIndex) {
+						regex.lastIndex++;
 					}
-					return {
-						name: file.name,
-						time: new Date(file.time)
-					}
-				});
-				let done = false;
-				for (let fileNum in files) {
-					if (done) break;
-					let file = files[fileNum];
-					if (file.time !== null) {
-						if (file.time.toUTCString() == new Date(filedate).toUTCString()) {
-							fs.readFile(path.join(logger.logPath, file.name), function(err, buffer) {
-								if (err) {
-									responseJSON.success = false;
-									responseJSON.status = 500;
-									responseJSON.message = err.toString();
-									logger.e("LoggingGet", "Request to fetch logs denied due to a file reading error", err);
-									responseSetting.setResponseFullJSON(response, responseJSON);
-									return;
-								}
-								let filestat = fs.statSync(logger.logPath + "/" + file.name);
-								let regex = new RegExp(`.*\\|\\ (?:${verbosity})\\ \\|\\ (?:${tag})\\ \\|\\ .*?(?:[\\n].*?)*(?={EOT})`, "g");
-								let fullLog = buffer.toString();
-								let m;
-								let fullResponse = [];
 
-								while ((m = regex.exec(fullLog)) !== null) {
-									if (m.index === regex.lastIndex) {
-										regex.lastIndex++;
-									}
-
-									m.forEach(function (match) {
-										if (match.length > 0) {
-											fullResponse.push(match);
-										}
-									});
-								}
-
-								let filteredResponse = fullResponse.slice(start, start + count);
-
-								responseJSON.success = true;
-								responseJSON.status = 200;
-								responseJSON.data = {
-									filename: file.name,
-									filedate: filestat.birthtime,
-									filesize: filestat.size,
-									count: filteredResponse.length,
-									list: filteredResponse
-								}
-								logger.v("LoggingGet", "Request to fetch logs processed");
-								responseSetting.setResponseFullJSON(response, responseJSON);
-								done = true;
-								return;
-							});
+					m.forEach(function (match) {
+						if (match.length > 0) {
+							fullResponse.push(match);
 						}
-					}
+					});
 				}
-				responseJSON.success = false;
-				responseJSON.status = 400;
-				responseJSON.message = "errrr";
-				logger.v("LoggingGet", "Request to fetch logs denied due to a file reading error", err);
+
+				let filteredResponse = fullResponse.slice(start, start + count);
+
+				responseJSON.success = true;
+				responseJSON.status = 200;
+				responseJSON.data = {
+					filename: filename,
+					filedate: filestat.birthtime,
+					filesize: filestat.size,
+					count: filteredResponse.length,
+					list: filteredResponse
+				}
+				logger.v("LoggingGet", "Request to fetch logs processed");
 				responseSetting.setResponseFullJSON(response, responseJSON);
+				done = true;
 				return;
 			});
 		});
