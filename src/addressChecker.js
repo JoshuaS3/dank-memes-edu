15612af80d078643f3412ae51547d076a3a005a3
Joshua <joshstockin@gmail.com>
2018-12-24 20:23:03 -0600

    Update addressChecker.js

--- src/addressChecker.js
+++ src/addressChecker.js
@@ -1,31 +1,9 @@
-module.exports = function (requestUrl, pageData) {
-	regexs = [];
-	regexs.push(
+module.exports = function (requestAddress, checkAddress) {
+	regex = 
 		new RegExp(
-			("^" + pageData.webAddress.replace(/\//g, "\\/") + "$"),
+			("^" + checkAddress.replace(/\//g, "\\/") + "$"),
 			"g"
-		)
-	);
-	
-	if (pageData.aliases) {
-		pageData.aliases.forEach(function (alias) {
-			regexs.push(
-				new RegExp(
-					("^" + alias.replace(/\//g, "\\/") + "$"),
-					"g"
-				)
-			);
-		});
-	}
+		);
 
-	let gotMatch = false;
-	regexs.forEach(function (regex) {
-		if (gotMatch) return;
-		if (requestUrl.match(regex)) {
-			gotMatch = true;
-			return;
-		};
-	});
-
-	return gotMatch;
+	return requestAddress.match(regex);
 }
